c56bc66145c76b4f92f403fb82ae8b81198b9780
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-16 16:45:21 +0000

    More CGL cleanup Removed function indices (as generated by extdeveval) Updated copyright year
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/vge_tagcloud/trunk@25477 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi2/class.tx_vgetagcloud_pi2.php
+++ pi2/class.tx_vgetagcloud_pi2.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2007 Francois Suter <typo3@cobweb.ch>
+*  (c) 2007-2009 Francois Suter <typo3@cobweb.ch>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -22,7 +22,7 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once(PATH_tslib . 'class.tslib_pibase.php');
 
 /**
  * Plugin to display list of pages for the 'vge_tagcloud' extension.
@@ -30,19 +30,6 @@ require_once(PATH_tslib.'class.tslib_pibase.php');
  * @author		Francois Suter <typo3@cobweb.ch>
  * @package		TYPO3
  * @subpackage	tx_vgetagcloud
- *
- */
-/**
- * [CLASS/FUNCTION INDEX of SCRIPT]
- *
- *
- *
- *   48: class tx_vgetagcloud_pi2 extends tslib_pibase
- *   62:     function main($content,$conf)
- *  122:     function init($conf)
- *
- * TOTAL FUNCTIONS: 2
- * (This index is automatically created/updated by the extension "extdeveval")
  *
  */
 class tx_vgetagcloud_pi2 extends tslib_pibase {
@@ -59,56 +46,56 @@ class tx_vgetagcloud_pi2 extends tslib_pibase {
 	 * @param	array		$conf: The plugin configuration
 	 * @return	string		The content that is displayed on the website
 	 */
-	function main($content,$conf) {
+	function main($content, $conf) {
 		$this->init($conf);
 
-// Get the selected keyword and apply stdWrap, if any
-
+			// Get the selected keyword and apply stdWrap, if any
 		$keyword = (empty($this->piVars['keyword'])) ? '' : $this->piVars['keyword'];
-		if (isset($this->conf['keyword.'])) $keyword = $this->cObj->stdWrap($keyword,$this->conf['keyword.']);
-
-// If no pages were passed, display error message
+		if (isset($this->conf['keyword.'])) {
+			$keyword = $this->cObj->stdWrap($keyword, $this->conf['keyword.']);
+		}
 
 		if (empty($this->piVars['pages'])) {
+			// If no pages were passed, display error message
+
 			$message = $this->pi_getLL('no_pages');
-			$message = sprintf($message,$keyword);
-			if (isset($this->conf['message.'])) $message = $this->cObj->stdWrap($message,$this->conf['message.']);
+			$message = sprintf($message, $keyword);
+			if (isset($this->conf['message.'])) {
+				$message = $this->cObj->stdWrap($message, $this->conf['message.']);
+			}
 			$content = $message;
-		}
+		} else {
+			// Else display resulting list of pages
 
-// Else display resulting list of pages
+			$pages = t3lib_div::trimExplode('_', $this->piVars['pages'], 1);
 
-		else {
-			$pages = t3lib_div::trimExplode('_',$this->piVars['pages'],1);
-
-// Get result message and apply stdWrap, if any
-
+				// Get result message and apply stdWrap, if any
+			$message = '';
 			if (count($pages) == 1) {
 				$message = $this->pi_getLL('related_single_page');
-				$message = sprintf($message, $keyword); // Message may contain 1 marker
-			}
-			else {
+					// Message may contain 1 marker
+				$message = sprintf($message, $keyword);
+			} else {
 				$message = $this->pi_getLL('related_pages');
-				$message = sprintf($message, count($pages), $keyword); // Message may contain 2 markers
+					// Message may contain 2 markers
+				$message = sprintf($message, count($pages), $keyword);
 			}
-			if (isset($this->conf['message.'])) $message = $this->cObj->stdWrap($message,$this->conf['message.']);
+			if (isset($this->conf['message.'])) {
+				$message = $this->cObj->stdWrap($message, $this->conf['message.']);
+			}
 			$content = $message;
 
-// Load page number as a comma-separated list in the data of the cObj
+				// Load page number as a comma-separated list in the data of the cObj
+			$this->cObj->data['tag_pages'] = implode(',', $pages);
 
-			$this->cObj->data['tag_pages'] = implode(',',$pages);
-
-// Perform rendering of the list of pages, as a cObj
-
-			$content .= $this->cObj->cObjGetSingle($this->conf['results'],$this->conf['results.']);
+				// Perform rendering of the list of pages, as a cObj
+			$content .= $this->cObj->cObjGetSingle($this->conf['results'], $this->conf['results.']);
 		}
 
-// Wrap the whole result, with baseWrap if defined, else with standard pi_wrapInBaseClass() call
-
+			// Wrap the whole result, with baseWrap if defined, else with standard pi_wrapInBaseClass() call
 		if (isset($this->conf['baseWrap.'])) {
-			return $this->cObj->stdWrap($content,$this->conf['baseWrap.']);
-		}
-		else {
+			return $this->cObj->stdWrap($content, $this->conf['baseWrap.']);
+		} else {
 			return $this->pi_wrapInBaseClass($content);
 		}
 	}
@@ -120,10 +107,10 @@ class tx_vgetagcloud_pi2 extends tslib_pibase {
 	 * @return	void
 	 */
 	function init($conf) {
-		$this->conf = $conf; // Base configuration is equal the the plugin's TS setup
+			// Base configuration is equal the the plugin's TS setup
+		$this->conf = $conf;
 
-// Load localized strings
-
+			// Load localized strings
 		$this->pi_loadLL();
 	}
 
