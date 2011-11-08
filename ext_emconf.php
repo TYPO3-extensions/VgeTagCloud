5db3739ec05011f3b305b9ac0aacef6d64968200
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-08 16:04:56 +0000

    Released to TER as version 1.6.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/vge_tagcloud/trunk@53906 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "vge_tagcloud".
 #
-# Auto generated 28-03-2011 22:04
+# Auto generated 08-11-2011 17:03
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,17 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.5.0',
+	'version' => '1.6.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '0.0.0-4.6.99'
+			'typo3' => '3.5.0-4.6.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"0a67";s:10:"README.txt";s:4:"fdee";s:33:"class.tx_vgetagcloud_listsdef.php";s:4:"4c76";s:32:"class.tx_vgetagcloud_realurl.php";s:4:"2cb8";s:12:"ext_icon.gif";s:4:"a9c5";s:17:"ext_localconf.php";s:4:"6947";s:15:"ext_php_api.dat";s:4:"212b";s:14:"ext_tables.php";s:4:"6393";s:15:"flexform_ds.xml";s:4:"075b";s:13:"locallang.xml";s:4:"2ce7";s:29:"locallang_csh_vgetagcloud.xml";s:4:"5d68";s:16:"locallang_db.xml";s:4:"91ad";s:14:"doc/manual.pdf";s:4:"5959";s:14:"doc/manual.sxw";s:4:"4817";s:43:"hooks/class.tx_vgetagcloud_hook_example.php";s:4:"d8d2";s:14:"pi1/ce_wiz.gif";s:4:"ef2a";s:32:"pi1/class.tx_vgetagcloud_pi1.php";s:4:"6066";s:40:"pi1/class.tx_vgetagcloud_pi1_wizicon.php";s:4:"76f1";s:14:"pi2/ce_wiz.gif";s:4:"ef2a";s:32:"pi2/class.tx_vgetagcloud_pi2.php";s:4:"7e1a";s:40:"pi2/class.tx_vgetagcloud_pi2_wizicon.php";s:4:"97d7";s:17:"pi2/locallang.xml";s:4:"5828";s:30:"res/images/tagcloud_scales.png";s:4:"ed38";s:20:"static/constants.txt";s:4:"1ba8";s:16:"static/setup.txt";s:4:"8832";}',
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"886b";s:10:"README.txt";s:4:"fdee";s:33:"class.tx_vgetagcloud_listsdef.php";s:4:"4c76";s:32:"class.tx_vgetagcloud_realurl.php";s:4:"2cb8";s:12:"ext_icon.gif";s:4:"a9c5";s:17:"ext_localconf.php";s:4:"6947";s:15:"ext_php_api.dat";s:4:"212b";s:14:"ext_tables.php";s:4:"6393";s:15:"flexform_ds.xml";s:4:"075b";s:13:"locallang.xml";s:4:"2ce7";s:29:"locallang_csh_vgetagcloud.xml";s:4:"5d68";s:16:"locallang_db.xml";s:4:"91ad";s:14:"doc/manual.pdf";s:4:"a6a8";s:14:"doc/manual.sxw";s:4:"7eab";s:43:"hooks/class.tx_vgetagcloud_hook_example.php";s:4:"d8d2";s:14:"pi1/ce_wiz.gif";s:4:"ef2a";s:32:"pi1/class.tx_vgetagcloud_pi1.php";s:4:"81a0";s:40:"pi1/class.tx_vgetagcloud_pi1_wizicon.php";s:4:"76f1";s:14:"pi2/ce_wiz.gif";s:4:"ef2a";s:32:"pi2/class.tx_vgetagcloud_pi2.php";s:4:"7e1a";s:40:"pi2/class.tx_vgetagcloud_pi2_wizicon.php";s:4:"97d7";s:17:"pi2/locallang.xml";s:4:"5828";s:30:"res/images/tagcloud_scales.png";s:4:"ed38";s:20:"static/constants.txt";s:4:"1ba8";s:16:"static/setup.txt";s:4:"15da";}',
 	'suggests' => array(
 	),
 );
