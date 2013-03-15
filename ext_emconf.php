10fecdd1dd8d8ff36fc851cab03ce5148f20476c
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-15 21:16:38 +0000

    Released to TER as version 1.7.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/vge_tagcloud/trunk@73011 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "vge_tagcloud".
-#
-# Auto generated 08-11-2011 17:03
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "vge_tagcloud".
+ *
+ * Auto generated 15-03-2013 22:15
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'A Better Tag Cloud',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.6.0',
+	'version' => '1.7.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-6.0.99',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"886b";s:10:"README.txt";s:4:"fdee";s:33:"class.tx_vgetagcloud_listsdef.php";s:4:"4c76";s:32:"class.tx_vgetagcloud_realurl.php";s:4:"2cb8";s:12:"ext_icon.gif";s:4:"a9c5";s:17:"ext_localconf.php";s:4:"6947";s:15:"ext_php_api.dat";s:4:"212b";s:14:"ext_tables.php";s:4:"6393";s:15:"flexform_ds.xml";s:4:"075b";s:13:"locallang.xml";s:4:"2ce7";s:29:"locallang_csh_vgetagcloud.xml";s:4:"5d68";s:16:"locallang_db.xml";s:4:"91ad";s:14:"doc/manual.pdf";s:4:"a6a8";s:14:"doc/manual.sxw";s:4:"7eab";s:43:"hooks/class.tx_vgetagcloud_hook_example.php";s:4:"d8d2";s:14:"pi1/ce_wiz.gif";s:4:"ef2a";s:32:"pi1/class.tx_vgetagcloud_pi1.php";s:4:"81a0";s:40:"pi1/class.tx_vgetagcloud_pi1_wizicon.php";s:4:"76f1";s:14:"pi2/ce_wiz.gif";s:4:"ef2a";s:32:"pi2/class.tx_vgetagcloud_pi2.php";s:4:"7e1a";s:40:"pi2/class.tx_vgetagcloud_pi2_wizicon.php";s:4:"97d7";s:17:"pi2/locallang.xml";s:4:"5828";s:30:"res/images/tagcloud_scales.png";s:4:"ed38";s:20:"static/constants.txt";s:4:"1ba8";s:16:"static/setup.txt";s:4:"15da";}',
+	'_md5_values_when_last_written' => 'a:27:{s:9:"ChangeLog";s:4:"598d";s:33:"class.tx_vgetagcloud_listsdef.php";s:4:"4c76";s:32:"class.tx_vgetagcloud_realurl.php";s:4:"2cb8";s:12:"ext_icon.gif";s:4:"a9c5";s:17:"ext_localconf.php";s:4:"6947";s:15:"ext_php_api.dat";s:4:"212b";s:14:"ext_tables.php";s:4:"e99d";s:15:"flexform_ds.xml";s:4:"a9d1";s:13:"locallang.xml";s:4:"2ce7";s:29:"locallang_csh_vgetagcloud.xml";s:4:"296e";s:16:"locallang_db.xml";s:4:"c4c8";s:10:"README.txt";s:4:"fdee";s:14:"doc/manual.pdf";s:4:"09bf";s:14:"doc/manual.sxw";s:4:"e869";s:43:"hooks/class.tx_vgetagcloud_hook_example.php";s:4:"4c47";s:14:"pi1/ce_wiz.gif";s:4:"ef2a";s:32:"pi1/class.tx_vgetagcloud_pi1.php";s:4:"e9fc";s:40:"pi1/class.tx_vgetagcloud_pi1_wizicon.php";s:4:"280c";s:14:"pi2/ce_wiz.gif";s:4:"ef2a";s:32:"pi2/class.tx_vgetagcloud_pi2.php";s:4:"ae64";s:40:"pi2/class.tx_vgetagcloud_pi2_wizicon.php";s:4:"cf61";s:17:"pi2/locallang.xml";s:4:"5828";s:27:"res/images/plugin1_icon.png";s:4:"84b3";s:27:"res/images/plugin2_icon.png";s:4:"7edb";s:30:"res/images/tagcloud_scales.png";s:4:"ed38";s:20:"static/constants.txt";s:4:"1ba8";s:16:"static/setup.txt";s:4:"3bae";}',
 	'suggests' => array(
 	),
 );
